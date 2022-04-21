54e7b69f3bbec5f32c7e5637e0819920be4fc1dc
IsaqueZaratustra <ADSisaque@gmail.com>
2022-04-21 17:17:37 -0300

    commit

--- pti.js
+++ pti.js
@@ -1,35 +1,99 @@
-var insiraGasolina = prompt("Insira o valor da Gasolina");
-
-var insiraAlcool = prompt ("Insira o valor do Alcool");
-
-if (insiraGasolina>70){
-  alert("Vale a pena Gasolina");
-}else{
-  if(insiraGasolina<70){
-    alert("vale a pena Álcool")  
-  }else{
-    if (insiraGasolina==insiraAlcool){
-      alert("tanto faz abastecer com um ou outro") 
-    }
-  }
+// Alertas iniciais com a temática do PTI
+alert("Gasolina ou Álcool?")
+alert("Descubra agora!")
+// Constantes que serão usadas no layout de saída
+const cabecalho= "╔════════════════════════════════════════╗";
+const divisor ="╠════════════════════════════════════════╣";
+const borda = "║";
+const rodape = "╚════════════════════════════════════════╝";
+// Variáveis do álcool e gasolina (com o valor inicial)
+var valAlcool = 0.00;
+var valGasolina = 0.00
+// Função genérica, para imprimir no console
+function print(s) {
+console.log(s);
 }
-
-var denovo = prompt("deseja fazer um novo calculo? s/n");
- if(denovo=="s"){
-   var insiraGasolina = prompt("Insira o valor da Gasolina");
-
-var insiraAlcool = prompt ("Insira o valor do Alcool");
-    if (insiraGasolina>70){
-  alert("Vale a pena Gasolina");
-}else{
-  if(insiraGasolina<70){
-    alert("vale a pena Álcool")  
-  }else{
-    if (insiraGasolina==insiraAlcool){
-      alert("tanto faz abastecer com um ou outro") 
-    }
-  }
+// Imprime o cabecalho de saída
+function printH(s) {
+print(cabecalho);
+printL("Posto Ipiranga")
+print(divisor);
 }
-} if (denovo=="n") {
-   alert("Até a próxima");
- }
+// Imprime o rodape de saída, do layout
+function printF(s) {
+print(rodape);
+}
+// Imprime uma linha dentro do layout
+function printL(s) {
+const l = s + " ".repeat(40 - s.length);
+print(borda + l + borda);
+}
+// Formata um número fixando três casas decimais
+function formataNumero(n) {
+return n.toFixed(3)
+}
+// Faz o cálculo para decidir o combustível e retorna a mensagem do combustivel
+function decideCombustivel() {
+const m = valAlcool / valGasolina;
+
+if (m == 0.7) {
+return "Tanto faz um ou outro."
+} else if (m > 0.7) {
+return "GASOLINA!"
+} else if (m < 0.7) {
+return "ÁLCOOL!"
+}
+}
+// Imprime o resultado montando a tela de resultado, com o layout do inicio do código
+function imprimeDados() {
+console.clear();
+printH();
+printL(" Gasolina: " + formataNumero(valGasolina));
+printL(" Alcool: " + formataNumero(valAlcool));
+printL("");
+printL(" abasteça com: " + decideCombustivel());
+printF();
+print("");
+}
+// Solicita o valor do combustivel, garantindo que a resposta seja um número
+function solicitaGasolina() {
+let s = prompt('Informe o valor da Gasolina: ');
+  s = s.replace(",",".");
+valGasolina = parseFloat(s);
+
+if (isNaN(valGasolina)) {
+  print("Você informou um número inválido. Tente novamente.");
+solicitaGasolina();
+}
+}
+// Solicita o valor do combustivel, garantindo que a resposta seja um número
+function solicitaAlcool() {
+let s = prompt('Informe o valor do Alcool: ');
+
+s = s.replace(",",".");
+valAlcool = parseFloat(s);
+
+if (isNaN(valAlcool)) {
+print("Você informou um número inválido. Tente novamente.");
+solicitaAlcool();
+}
+}
+// Reinicia o programa
+function restart() {
+const resposta = prompt("Deseja refazer os cálculos? (s/n)");
+if (resposta.toLowerCase() == "s") {
+start();
+} else if (resposta.toLowerCase() != "n") {
+print("Resposta inválida. Tente novamente.");
+restart();
+}
+}
+// Função com as etapas do programa a serem realizadas.
+function start(){
+solicitaGasolina();
+solicitaAlcool();
+imprimeDados();
+restart();
+}
+// Inicia o programa
+start();
