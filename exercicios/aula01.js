fef3a30e256d59f2b390b2e8c1ba4d2541a730be
IsaqueZaratustra <ADSisaque@gmail.com>
2022-05-07 16:57:14 -0300

    add

---
+++ exercicios/aula01.js
@@ -0,0 +1,9 @@
+remetente = prompt("digite o remetente")
+
+destinatario = prompt("digite o destinatário")
+
+alert("Remetente: " + remetente)
+
+alert("Destinatario: " + destinatario)
+
+alert("Mensagem: Gosto muito de Você")
